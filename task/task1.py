9c5f415292ddfbf5f7abf7a16349fdf5df21c1c8
Deanisgarayev <garaevdenis2019@gmail.com>
2024-12-29 15:17:15 +0500

    in progress

--- task/task1.py
+++ task/task1.py
@@ -14,13 +14,15 @@ driver = webdriver.Chrome(options=option)
 
 def s():
     default
-    # sleep(1)
+    sleep(1)
 
 def set_up():
+    '''gets url and maximizes window'''
     driver.get('http://www.saucedemo.com/')
     driver.maximize_window()
 
 def login():
+    '''enters correct name and password. And clicks to button login'''
     user_name = driver.find_element(By.XPATH,'//*[@id="user-name"]')
     user_name.send_keys('standard_user')
     file.write('Success write name\n')
@@ -32,9 +34,10 @@ def login():
     button = driver.find_element(By.XPATH,'//*[@id="login-button"]')
     button.click()
     file.write('Success login\n')
-
+    s()
 
 def choose_goods():
+    '''adds goods to cart'''
     good1_b = driver.find_element(By.XPATH,'//*[@id="add-to-cart-sauce-labs-backpack"]')
     good1_b.click()
     file.write('Success add first good\n')
@@ -46,18 +49,24 @@ def choose_goods():
     good3_b = driver.find_element(By.XPATH,'//*[@id="add-to-cart-test.allthethings()-t-shirt-(red)"]')
     good3_b.click()
     file.write('Success add first third\n')
+    s()
 
 def enter_to_the_cart():
+    '''clicks to button cart'''
     cart_b = driver.find_element(By.XPATH,'//*[@id="shopping_cart_container"]/a')
     cart_b.click()
     file.write('Success enter_to_the_cart\n')
+    s()
 
 def remove_good_in_the_cart():
+    '''removes good from cart'''
     remove_b = driver.find_element(By.XPATH, '//*[@id="remove-sauce-labs-fleece-jacket"]')
     remove_b.click()
     file.write('Success remove_good_in_the_cart\n')
+    s()
 
 def checkout():
+    '''clicks button checkout. Enters first name, last name and code. After clicks button continue'''
     checkout_b = driver.find_element(By.XPATH, '//*[@id="checkout"]')
     checkout_b.click()
     file.write('Success checkout\n')
@@ -77,18 +86,24 @@ def checkout():
     continue_b = driver.find_element(By.XPATH, '//*[@id="continue"]')
     continue_b.click()
     file.write('Success continue\n')
+    s()
 
 def finish():
+    '''clicks button finish'''
     finish_b = driver.find_element(By.XPATH,'//*[@id="finish"]')
     finish_b.click()
     file.write('Success finish\n')
+    s()
 
 def back_home():
+    '''clicks button back home'''
     back_home_b = driver.find_element(By.XPATH,'//*[@id="back-to-products"]')
     back_home_b.click()
     file.write('Success back home\n')
+    s()
 
 def logout():
+    '''clicks button menu and clicks button logout'''
     menu_b = driver.find_element(By.XPATH,'//*[@id="react-burger-menu-btn"]')
     menu_b.click()
     sleep(1)
@@ -98,6 +113,7 @@ def logout():
 
 
 def test_context_when_finished():
+    '''checks context: Thank you for your order!'''
     correct_text = "Thank you for your order!"
     current_text = driver.find_element(By.XPATH,'//*[@id="checkout_complete_container"]/h2')
 
@@ -105,6 +121,7 @@ def test_context_when_finished():
     file.write("test_context_when_finished is ok\n")
 
 def test_chosen_correct_goods():
+    '''checks chosen correct goods and makes screenshot'''
     correct_good1 = 'Sauce Labs Backpack'
     correct_good2 = 'Test.allTheThings() T-Shirt (Red)'
     current_good1 = driver.find_element(By.XPATH,'//*[@id="item_4_title_link"]/div')
@@ -120,6 +137,7 @@ def test_chosen_correct_goods():
     file.write('test_chosen_correct_good 2 is ok\n')
 
 def test_total_price():
+    '''checks correct total price'''
     correct_total_price ='Total: $49.66'
     current_total_price = driver.find_element(By.XPATH,'//*[@id="checkout_summary_container"]/div/div[2]/div[8]')
 
@@ -127,6 +145,7 @@ def test_total_price():
     file.write('test_total_price is ok\n')
 
 def test_logout_redirect():
+    '''checks that logouts correct and gets correct URL'''
     correct_url="https://www.saucedemo.com/"
     get_url=driver.current_url
 
@@ -134,23 +153,23 @@ def test_logout_redirect():
     file.write("test_logout_redirect is ok\n")
 
 set_up()
-s()
+
 login()
-s()
+
 choose_goods()
-s()
+
 enter_to_the_cart()
-s()
+
 remove_good_in_the_cart()
 test_chosen_correct_goods()
-s()
+
 checkout()
 test_total_price()
-s()
+
 finish()
 test_context_when_finished()
-s()
+
 back_home()
-s()
+
 logout()
 test_logout_redirect()
